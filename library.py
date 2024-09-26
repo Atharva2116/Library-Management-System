3cd3e5b78e522d6aed8d2e9530c0542bc9c12fb7
Atharva2116 <atharvadandgawhal.2002@com>
2024-09-27 00:26:14 +0530

    Implement borrow book feature

--- library.py
+++ library.py
@@ -8,6 +8,15 @@ class Book:
 class Library:
     def __init__(self):
         self.books_available = []
+        self.borrowed_books = []
 
     def add_book(self, book):
         self.books_available.append(book)
+
+    def borrow_book(self, isbn):
+        for book in self.books_available:
+            if book.isbn == isbn:
+                self.books_available.remove(book)
+                self.borrowed_books.append(book)
+                return True
+        return False
